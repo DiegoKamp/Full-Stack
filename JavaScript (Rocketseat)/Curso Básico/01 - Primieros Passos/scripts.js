7f1ede9a0af254dc6535ee7a7284a8a30ec7bb96
DiegoKamp <diegokamp@gmail.com>
2025-12-05 16:36:03 -0300

    Teste de contribuição

---
+++ JavaScript (Rocketseat)/Curso Básico/01 - Primieros Passos/scripts.js
@@ -0,0 +1,9 @@
+/*Conhecendo o JavaScript*/
+
+/*É uma linguagem de programação que permite implementar interatividade e funcionalidade nas páginas da web.
+É client-side*/
+
+/*Anexar o arquivo JavaScript na última linha do body no index.html*/
+
+console.log("Diego Kamp aqui!"); // Confirmação de conexão
+
