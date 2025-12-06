03859bf3da24a8a3fe1151a98a7f36521867da38
DiegoKamp <diegokamp@gmail.com>
2025-12-06 16:05:06 -0300

    Até Ordem de Precedência e Grouping Operator

---
+++ JavaScript (Rocketseat)/Curso Básico/03 - Expressões e Operadores/scripts.js
@@ -0,0 +1,80 @@
+/*Expressões e Operadores*/
+
+
+// Operadores aritméticos
+console.log("Soma:", 12 + 9);
+console.log("Concatenação:", "12" + 9);
+
+console.log("Subtração:", 12 - 9);
+
+console.log("Multiplicação:", 12*9);
+
+console.log("Divisão:", 12/9);
+
+console.log("Resto da divisão:", 12%9);
+
+console.log("Potenciação:", 12**9);
+
+
+// Incremento e Decremento
+
+let number = 5;
+
+console.log(number);
+
+// Incremento
+number = number + 1;
+
+console.log(number);
+
+number++;
+
+console.log(number);
+
+console.log(number++); // O incremento é feito após aparecer no console.
+
+console.log(number);
+
+console.log(++number); // O incremento é feito antes do console, por isso aparecerá
+
+
+// Decremento
+number = number -1;
+console.log(number);
+
+number--;
+
+console.log(number);
+
+console.log(number--); // O decremento é feito após aparecer no console.
+
+console.log(number);
+
+console.log(--number); // O incremento é feito antes do console, por isso aparecerá
+
+number += 30;
+
+console.log(number);
+
+number -= 29;
+
+console.log(number);
+
+number *=2;
+
+console.log(number);
+
+number **=2;
+
+console.log(number);
+
+number /=3;
+
+console.log(number);
+
+// Ordem de Precedência
+
+//https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Operators/Operator_precedence
+
+
+
