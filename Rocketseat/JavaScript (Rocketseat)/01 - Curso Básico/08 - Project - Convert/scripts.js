d20f7eba52cdc2eef74a6418e0d73d9555d74efc
DiegoKamp <diegokamp@gmail.com>
2026-01-13 14:14:26 -0300

    Obtendo o input e aplicando o regex para permitir entrada apenas de números

--- Rocketseat/JavaScript (Rocketseat)/01 - Curso Básico/08 - Project - Convert/scripts.js
+++ Rocketseat/JavaScript (Rocketseat)/01 - Curso Básico/08 - Project - Convert/scripts.js
@@ -2,4 +2,18 @@
 console.log("Olá, Diego aqui!")
 
 /*Recuperando os elementos do index*/
+const amount = document.getElementById("amount");
+
+
+
+
+
+// Manipulando o input do amount para receber somente números
+amount.addEventListener("input", () => {
+  // console.log(amount.value) checando se estou vendo o evento
+
+  const hasCharactersRegex = /\D+/g
+  amount.value = amount.value.replace(hasCharactersRegex, "") // Recebe o próprio amount, mas somente números
+
+});
 
